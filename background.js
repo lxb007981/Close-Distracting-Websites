f9074fbed708165090763b6b08c9682a82f7bdb8
lxb007981 <lxb007981@hotmail.com>
2021-11-28 17:59:40 +0800

    migrate to manifest V3, using Alarm and Storage

--- background.js
+++ background.js
@@ -1,29 +1,55 @@
-let threshold = 1000 * 60 * 20;
-let urlList = ['https://www.bilibili.com/*', 'https://www.youtube.com/*', 'https://www.zhihu.com/*']
-console.log(`Block list: ${urlList}. Threshold: ${threshold}`);
-
-let timer = null;
-chrome.runtime.onMessage.addListener(
-  function(request, _sender, sendResponse) {
-    if ('threshold' in request){
-      threshold = request.threshold;
-      urlList = request.urlList;
-      console.log(`Updated block list: ${urlList} and threshold: ${threshold}`);  
-    } else {
-      sendResponse({threshold: parseInt(threshold/1000/60), urlList: urlList});
-    }
+const SCANNING_INTERVAL = 2;
+let threshold = 10;
+let urlList = ['https://www.bilibili.com/*', 'https://www.youtube.com/*', 'https://www.zhihu.com/*'];
+chrome.storage.local.get(["threshold", "urlList"], (object) => {
+  if (!('threshold' in object)) {
+    chrome.storage.local.set({ threshold });
+    chrome.storage.local.set({ urlList });
+    console.log(`Block list: ${urlList}. Threshold: ${threshold}`);
   }
-);
+})
 
-setInterval(async () => {
-  if (timer === null) {
-    let tabs = await chrome.tabs.query({ url: urlList });
+chrome.storage.onChanged.addListener(async () => {
+  let destroyer = await chrome.alarms.get("destroyer");
+  if (destroyer != null) {
+    let [_, newUrlList, newThreshold] = await Promise.all([chrome.alarms.clear(destroyer.name),
+    chrome.storage.local.get(["urlList"]),
+    chrome.storage.local.get(["threshold"])]);
+    let tabs = await chrome.tabs.query({ url: newUrlList.urlList });
     if (tabs.length !== 0) {
-      timer = setTimeout((async () => {
-        let tabsToDestroyed = await chrome.tabs.query({ url: urlList });
-        await chrome.tabs.remove(tabsToDestroyed.map(tab => tab.id));
-      }), threshold);
+      chrome.alarms.create("destroyer", { delayInMinutes: newThreshold.threshold });
+      console.log(`Destroy in ${newThreshold.threshold} minutes`);
     }
-
   }
-}, 1000 * 60 * 5)
+});
+chrome.alarms.get("scanner", (scanner) => {
+  if (scanner == null) {
+    chrome.storage.local.get(["threshold"], (object) => {
+      chrome.alarms.create("scanner", { delayInMinutes: object.threshold, periodInMinutes: SCANNING_INTERVAL });
+    });
+  }
+})
+chrome.alarms.onAlarm.addListener(async (alarm) => {
+  if (alarm.name === "scanner") {
+    let destroyer = await chrome.alarms.get("destroyer");
+    if (destroyer == null) {
+      let getValue = await chrome.storage.local.get(["threshold", "urlList"]);
+      let newThreshold = getValue.threshold, newUrlList = getValue.urlList;
+      scanTabs(newThreshold, newUrlList);
+    }
+  }
+  else {//alarm.name==="destroyer"
+    let getValue = await chrome.storage.local.get(["urlList"]);
+    let tabsToDestroyed = await chrome.tabs.query({ url: getValue.urlList });
+    chrome.tabs.remove(tabsToDestroyed.map(tab => tab.id));
+  }
+});
+async function scanTabs(threshold, urlList) {
+  console.log("Scanning tabs...")
+  let tabs = await chrome.tabs.query({ url: urlList });
+  if (tabs.length !== 0) {
+    chrome.alarms.create("destroyer", { delayInMinutes: threshold });
+    console.log(`Destroy in ${threshold} minutes`);
+  }
+
+}
--- options.js
+++ options.js
@@ -1,24 +1,18 @@
 let thresholdNode = document.getElementById("threshold");
 let blocklistNode = document.getElementById("blocklist");
 let responseNode = document.getElementById("response");
-chrome.runtime.sendMessage({},(response)=>{
-    let threshold = response.threshold;
-    let urlList = response.urlList;
-    thresholdNode.value = threshold;
-    blocklistNode.value = urlList.join('\n');
-});
+
+chrome.storage.local.get(["threshold"], ({ threshold }) => { thresholdNode.value = threshold; })
+chrome.storage.local.get(["urlList"], ({ urlList }) => { blocklistNode.value = urlList.join('\n'); })
 document.getElementById("submitButton").addEventListener("click", set);
 document.getElementById("clearButton").addEventListener("click", clearInput);
 function set() {
-    let threshold = thresholdNode.value.trim();
+    let threshold = parseInt(thresholdNode.value.trim());
     let urlList = blocklistNode.value.trim().split('\n');
     responseNode.innerHTML = "Submitted!"
-    try {
-        chrome.runtime.sendMessage({threshold: parseInt(threshold)*60*1000, urlList: urlList});
-    } catch (error) {
-        console.log(error)
-    }
-    
+
+    chrome.storage.local.set({ threshold });
+    chrome.storage.local.set({ urlList });
 }
 function clearInput() {
     thresholdNode.value = "";
